3ef2eda5f8ee84e18a9cb3ee83e80b36bd172e6f
Ssanti66 <santiagosaucedo66@gmail.com>
2023-06-10 12:06:32 -0300

    updated

--- practicaFuncional/practicaParcial2022.hs
+++ practicaFuncional/practicaParcial2022.hs
@@ -1,103 +1,152 @@
--- Un candidato tiene nombre, edad, carisma y capacidades. Las capacidades sirven para convencer a la gente (mejor explicado más adelante).
-
-
-import Text.Show.Functions()
-import Data.Bool (bool)
-
-
 data Candidato = Candidato {
-    nombre :: String,
-    edad :: Double,
-    carisma :: Double,
-    capacidades :: [Capacidad]
+    nombre:: String,
+    edad:: Float,
+    carisma:: Float,
+    capacidades:: [Capacidad]
 } deriving Show
+
+type Capacidad = Candidato -> Float
+
+incapaz = Candidato "Inca" 40 100 [corrupto]
 
 -- 1
--- Las capacidades del candidato para convencer son:
+-- 1.a)
 -- facha: (60 - edad del candidato)  + carisma del candidato * 3
--- liderazgo: edad del candidato * 10
--- riqueza: carisma del candidato + edad del candidato / 50
--- corrupto: resta 100 (en este caso no ayuda a convencer)
--- tiktoker: 100
--- -- flogger: 0 ( ya pasó de moda (?) )
-
-type Capacidad = Candidato -> Double
-
-facha :: Capacidad
+facha:: Capacidad
 facha candidato = (60 - edad candidato) + (carisma candidato * 3)
 
-liderazgo :: Capacidad
-liderazgo = (*10) . edad
+-- 1.b)
+-- liderazgo: edad del candidato * 10
+-- aplicación parcial en el (*10)
+-- composición entre (*10) y edad
+liderazgo:: Capacidad
+liderazgo = (*10).edad
 
-riqueza :: Capacidad
-riqueza candidato= carisma candidato + (edad candidato / 50)
+-- 1.c)
+-- riqueza: carisma del candidato + edad del candidato / 50
+riqueza:: Capacidad
+riqueza candidato = carisma candidato + (edad candidato / 50)
 
-corrupto :: Capacidad
-corrupto _ = -100
+-- 1.d)
+-- corrupto: resta 100 (en este caso no ayuda a convencer)
+corrupto:: Capacidad
+corrupto _ = -100.0
 
-tiktoker :: Capacidad
+-- 1.e)
+-- tiktoker: 100
+tiktoker:: Capacidad
 tiktoker _ = 100
 
-flogger :: Capacidad
-flogger _ = 0
+-- 1.f)
+-- flogger: 0 ( ya pasó de moda (?) )
+flogger:: Capacidad
+flogger _ = 0 
 
-candidatoPrueba :: Candidato
-candidatoPrueba = Candidato "Santiago" 22 25 [facha]
+-- 2)
+-- 2.a)
+-- Cintia, que tiene 40 años, su carisma es de 12 y las capacidades
+-- que tiene son liderazgo, riqueza y es tiktoker.
+cintia = Candidato {
+    nombre = "Cintia",
+    edad = 40,
+    carisma = 12,
+    capacidades = [liderazgo, riqueza, tiktoker]
+}
 
--- Modelar a los siguientes candidatos:
--- Cintia, que tiene 40 años, su carisma es de 12 y las capacidades que tiene son liderazgo, riqueza y es tiktoker.
--- Marcos, que tiene 45 años, su carisma es de 10 y las capacidades que tiene son facha, liderazgo, corrupto.
+-- 2.b)
+-- Marcos, que tiene 45 años, su carisma es de 10 y las capacidades
+-- que tiene son facha, liderazgo, corrupto.
+marcos = Candidato {
+    nombre = "Marcos",
+    edad = 45,
+    carisma = 10,
+    capacidades = [facha, liderazgo, corrupto]
+}
 
-cintia :: Candidato
-cintia = Candidato "Cintia" 40 12 [liderazgo, riqueza, flogger]
+-- 3)
+-- Se desea saber si una persona tiene capacidades inútiles,
+-- se cumple cuando la persona tiene al menos una capacidad
+-- con convencimiento menor o igual a 0.
+tieneCapacidadInutil:: Candidato -> Bool
+tieneCapacidadInutil candidato =
+    any (esInutilPara candidato) (capacidades candidato)
 
-marcos :: Candidato
-marcos = Candidato "Marcos" 45 10 [facha, liderazgo]
+esInutilPara candidato capacidad = (capacidad candidato) <= 0
 
--- Se desea saber si una persona tiene capacidades inútiles, se cumple cuando la persona tiene al menos una capacidad con convencimiento menor o igual a 0.
--- 	ej: tieneCapacidadInutil marcos 
---                 True
+-- 4)
+-- Saber los candidatos que entre sus capacidades 
+-- no tienen ninguna que sea inútil.
+losNoInutiles:: [Candidato] -> [Candidato]
+losNoInutiles candidatos = filter (not.tieneCapacidadInutil) candidatos
 
+-- 5)
+-- Se desea poder evaluar las capacidades de un candidato y
+-- obtener el valor de la suma de su convencimiento.
+-- Se debe devolver un candidato evaluado: (nombre, sumaCapacidades).
+-- Si la suma es menor a 0 debe devolver 0.
+evaluarCandidato:: Candidato -> (String, Float)
+evaluarCandidato candidato = 
+    (nombre candidato,
+    convencimiento candidato)
 
-tieneCapacidadesInutiles :: Candidato -> Bool
-tieneCapacidadesInutiles candidato = any (<=0)  (map (aplicarCapacidad candidato) (capacidades candidato))
+convencimiento candidato = max 0 (sum (map ($candidato) (capacidades candidato)))
 
-aplicarCapacidad candidato capacidad = capacidad candidato
+-- 6)
+-- Definir la función “elMejor/3” que dado dos elementos
+-- y una operación define cuál es el mejor de los dos.
+-- Se considera que un elemento es mejor que otro, si
+-- la operación a ese elemento, éste tiene un mayor resultado
+-- que el segundo. Se espera como resultado el elemento.
+-- En caso de empate devuelve el primero.
+elMejor:: Ord b => (a -> b) -> a -> a -> a 
+elMejor criterio primero segundo
+    | criterio primero >= criterio segundo = primero
+    | otherwise = segundo
 
--- Saber los candidatos que entre sus capacidades no tienen ninguna que reste puntos o que no hagan nada.
+-- 7)
+-- Definir la función “votacion/2” que dada una cantidad
+-- de votantes y una lista de candidatos devuelve una
+-- nueva lista con el nombre del candidato y la cantidad
+-- de votos que consiguió. La cantidad de votos se define por:
+-- cantidadVotantes * convencimientoCandidato / totalConvencimientoDeTodosLosCandidatos.
+votacion cantidadVotos candidatos =
+    map (resultadoVotacion cantidadVotos (totalConvencimientoDe candidatos)) candidatos
 
-tienenCapacidadesUtiles :: [Candidato] -> [Candidato]
-tienenCapacidadesUtiles candidatos = filter (not . tieneCapacidadesInutiles) candidatos
+resultadoVotacion cantidadVotos convencimientoTotal candidato =
+    (nombre candidato,
+    calcularVotos cantidadVotos convencimientoTotal candidato)
 
--- Se desea poder evaluar las capacidades de un candidato y obtener el valor de la suma de su convencimiento. Se debe devolver un candidato evaluado: (nombre, sumaCapacidades). Si la suma es menor a 0 debe devolver 0.
--- 	ej: evaluarCandidato cintia
---        (“cintia”, 512.8)	(400 + 12 + 0.8 + 100)
+calcularVotos cantidadVotos convencimientoTotal candidato =
+    cantidadVotos * convencimiento candidato / convencimientoTotal
 
-data CandidatoEvaluado = CandidatoEvaluado {
-    nombre2 :: String,
-    sumaCapacidades :: Double
-} deriving Show
+totalConvencimientoDe = sum . map convencimiento
+-- 8)
+-- 8.a)
+-- “ganador/2”: Dada una cantidad de votantes y una lista
+-- de candidatos, nos informa cuál fue el ganador de la elección
+-- (sería el que tiene mayor proporción de votos).
 
-evaluarCandidato :: Candidato -> CandidatoEvaluado
-evaluarCandidato candidato = CandidatoEvaluado (nombre candidato) (sumarCapacidades candidato)
+ganador cantidadVotos candidatos =
+    elMejorDeTodos
+        (calcularVotos cantidadVotos (totalConvencimientoDe candidatos))
+        candidatos
+--    foldl1 
+--        (elMejor (calcularVotos cantidadVotos (totalConvencimientoDe candidatos)))
+--        candidatos
 
-sumarCapacidades candidato = foldl (\acumulador capacidad -> acumulador + aplicarCapacidad candidato capacidad) 0 (capacidades candidato)
+-- 8.b)
+-- “elMasFachero/1”: Dada una lista de candidatos es el que
+-- tiene más facha (no importa que no esté entre las capacidades).
 
--- Definir la función “elMejor/3” que dado dos elementos y una operación define cuál es el mejor de los dos. Se considera que un elemento es mejor que otro, si la operación a ese elemento, éste tiene un mayor resultado que el segundo. Se espera como resultado el elemento. En caso de empate devuelve el primero.
+elMasFachero candidatos = elMejorDeTodos facha candidatos
+-- foldl1 (elMejor facha) candidatos
 
-elMejorDe3 :: Candidato -> Candidato -> Capacidad -> Candidato
-elMejorDe3 candidato1 candidato2 capacidadAAplicar 
-    | (aplicarCapacidad candidato1 capacidadAAplicar) >= (aplicarCapacidad candidato2 capacidadAAplicar) = candidato1
-    | otherwise = candidato2
+-- 8.c)
+-- “elMasMolestoDeNombrar/1”: Es el candidato con
+-- más letras en su nombre.
 
--- Definir la función “votacion/2” que dada una cantidad de votantes y una lista de candidatos devuelve una nueva lista con el nombre del candidato y la cantidad de votos que consiguió.
--- La cantidad de votos se define por: cantidadVotantes * convencimientoCandidato / totalConvencimientoDeTodosLosCandidatos.
+elMasMolestoDeNombrar candidatos = elMejorDeTodos (length.nombre) candidatos
+-- foldl1 (elMejor (length.nombre)) candidatos
 
-votacion2 :: Double -> [Candidato] -> [(String, Double)]
-votacion2 cantidadVotantes candidatos = map (calcularVotos cantidadVotantes candidat) candidatos
-
-
-calcularVotos cantidadVotantes candidato = (nombre candidato, (fromIntegral cantidadVotantes * sumarCapacidades candidato / totalConvencimiento))
-
-totalConvencimiento = sum (map sumarCapacidades candidatos)
-
+elMejorDeTodos criterio candidatos =
+    foldl1 (elMejor criterio) candidatos
